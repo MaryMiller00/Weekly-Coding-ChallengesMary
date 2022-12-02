c7e2b5535a00052dd958a7938c66525e9e7a4d36
MaryMiller00 <mary092@revature.net>
2022-12-02 11:13:30 -0500

    final commit

--- week3/problem1/src/main/java/com/revature/week3/Solution.java
+++ week3/problem1/src/main/java/com/revature/week3/Solution.java
@@ -4,6 +4,26 @@ import java.util.*;
 
 public class Solution {
     public boolean isAnagram(String s1, String s2) {
-        throw new RuntimeException("Todo! Needs implementation...");
+        
+        
+        
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        int index[] = new int[256];//amount of characters
+
+        for(int i = 0; i<s1.length(); i++){
+            index[s1.charAt(i)]++;
+            index[s2.charAt(i)]--;
+        }
+        for (int i = 0; i< 256; i++){
+            if(index[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    //throw new RuntimeException("Todo! Needs implementation...");
     }
 }
+
+
--- week3/problem2/src/main/java/com/revature/week3/Solution.java
+++ week3/problem2/src/main/java/com/revature/week3/Solution.java
@@ -4,6 +4,45 @@ import java.util.*;
 
 public class Solution {
     public int validWordsCount(String sentence) {
-        throw new RuntimeException("Todo! Needs implementation...");
+        //throw new RuntimeException("Todo! Needs implementation...");
+       int valid = 0;
+       //valid words
+       //hypens
+       int hyphen = 0;
+       int wordCount = 0;
+       char[] array = sentence.toCharArray();
+       //loop for seeing every letter
+       for(int i = 0; i< array.length; i++){
+        //temp
+        char temp = array[i];
+        if(temp==' '){
+            valid++;
+        }
+        //return back to 0s
+        hyphen = 0;
+        wordCount = 0;
+        i = next(array, i);
+        continue;
+       }
+
+       if(Character.isDigit(temp)||hypen >= 2 || wordCount == 0||i - 1 >= 0 && (array[index - 1] < 'a' || array[index - 1] > 'z') || i + 1 < array.length && (array[i + 1] < 'a' || array[i + 1] > 'z')|| index + 1 < arr.length && (array[i + 1] < 'a' || array[i + 1] > 'z')||
+       i + 1 < array.length && array[i + 1] != ' ' ){
+      //go ahead and reset the values instead of continueing with the word
+        hypen = 0;
+        wordCount = 0;
+        i = next(array, i);
+        continue;
+       }
+
+
     }
+    private static int next(char[] array, int index){
+        //there is a space before each word
+
+        for(int i = index; i<array.length && array[i] != ' '; i++){
+            index = i;
+        }
+        return i;
+    }
+
 }
